c23fceee1252fcdf6ede3002d3bc85f9fe756eaa
Jimmy Dinh <112448120+jimdcodes@users.noreply.github.com>
2024-10-05 02:31:02 +0000

    Learning Chapter 4

--- Chapter 3/WorkingWithDates/SeparateDatesAlias.sql
+++ Chapter 3/WorkingWithDates/SeparateDatesAlias.sql
@@ -2,3 +2,6 @@ SELECT Year(CreationDate) as OrderYear,
   Month(CreationDate) as OrderMonth,
   Day(CreationDate) as OrderDay
 FROM Orders
+WHERE Year(CreationDate) > '2015'
+AND MONTH(CreationDate) > '6'
+AND DAY(CreationDate) > '15'
